07461a347706df620af59807c5307d1efb550484
khankhanfauzan <khankhanfauzan@gmail.com>
2026-09-25 22:37:47 +0800

    feat: add frontend URL to CORS origin list

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
       'http://localhost:3000',
       /^http:\/\/localhost:\d+$/,
       'null',
+      "https://vi-mamabear-frontend.vercel.app",
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
